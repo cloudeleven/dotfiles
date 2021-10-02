499de68d2c2283af3f2cae34c8d1314c373b76f0
cloudeleven <nt@cloudeleven.space>
2021-10-03 05:38:11 +0900

    add hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,15 @@
+-- ターミナルでctrl + j
+local function terminalEvent(name, event, app)
+  if event == hs.application.watcher.activated then
+    if name == 'Terminal' or name == 'iTerm2' then
+        hs.hotkey.bind({"ctrl"}, "j", function()
+            hs.eventtap.keyStroke({}, 104, 0)
+        end)
+    else
+        hs.hotkey.disableAll("ctrl", "j")
+    end
+  end
+end
+
+terminalWatch = hs.application.watcher.new(terminalEvent)
+terminalWatch:start()
